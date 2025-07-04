00c42ec4cd27ba66eaa02744f0a4f458443a60e7
Karen Valentim <karenvalentim54@gmail.com>
2025-07-03 22:24:55 -0300

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ function App() {
 
   return (
     <>
-      
-      <h1>Olá Mundo</h1>
-    
+      <div className="card">
+        <h1 className='card-title'>Rick Sanchez</h1>
+
+        <span className='tag'>Status: Vivo</span>
+        <span className='tag'>Espécie: Humana</span>
+        <span className='tag'>Origem: Terra C-137</span>
+
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" className='card-image' />
+      </div>
+
     </>
   )
 }
